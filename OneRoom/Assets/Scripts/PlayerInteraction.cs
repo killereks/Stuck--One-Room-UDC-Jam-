554f9edd54b4d31d986bae248426ce57124c791b
killereks <ssneeff@gmail.com>
2021-09-04 15:16:16 +0100

    init, add hierarchy, cinemachine, naughtyattributes

---
+++ OneRoom/Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayerInteraction : MonoBehaviour {
+
+    [BoxGroup("General Settings")]
+    public Camera cam;
+
+    [Range(0.5f, 3f), BoxGroup("Gameplay Settings")]
+    public float interactionDistance = 2f;
+
+    [BoxGroup("UI")]
+    public GameObject interactionUI;
+    [BoxGroup("UI")]
+    public TMPro.TextMeshProUGUI interactionText;
+
+    public static PlayerInteraction instance;
+
+    private void Start() {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update(){
+        InteractionRay();
+    }
+
+    void InteractionRay() {
+        Vector2 screenMiddle = new Vector2(Screen.width/2f, Screen.height/2f);
+
+        Ray ray = cam.ScreenPointToRay(screenMiddle);
+        RaycastHit hit;
+
+        bool hitSomething = false;
+
+        // CONSIDER USING A MASK/LAYER
+        if (Physics.Raycast(ray, out hit, interactionDistance)) {
+            IInteractable interactable = hit.collider.gameObject.GetComponent<IInteractable>();
+
+            if (interactable != null) {
+                hitSomething = true;
+                interactionText.text = interactable.GetDescription();
+                // USE INPUT MANAGER
+                if (Input.GetKeyDown(KeyCode.E)) {
+                    interactable.Interact();
+                }
+            }
+        }
+
+        interactionUI.SetActive(hitSomething);
+    }
+}
---
+++ OneRoom/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour {
+
+    [BoxGroup("Player Settings")]
+    public float walkingSpeed;
+    [BoxGroup("Player Settings")]
+    public float gravityScale;
+    [BoxGroup("Object Settings")]
+    public Transform playerT;
+
+    Rigidbody rb;
+
+    [BoxGroup("Object Settings")]
+    public Cinemachine.CinemachineVirtualCamera cam;
+
+    float cameraXRotation;
+    [BoxGroup("Camera Settings")]
+    public Vector2 cameraClamp;
+    [BoxGroup("Camera Settings")]
+    public float mouseSens = 2f;
+
+    bool canMove;
+
+    Collider collider;
+
+    float footstepTime;
+    public float footstepInterval = 4f;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody>();
+
+        collider = GetComponent<Collider>();
+
+        LockCursor(true);
+        canMove = true;
+    }
+
+    public void LockCursor(bool newState) {
+        Cursor.lockState = newState ? CursorLockMode.Locked : CursorLockMode.None;
+        Cursor.visible = !newState;
+
+        ToggleMovement(newState);
+    }
+    
+    public void ToggleMovement(bool newState) {
+        canMove = newState;
+
+        if (!canMove) rb.velocity = Vector3.zero;
+    }
+
+    private void Update() {
+        cam.m_Lens.FieldOfView = Mathf.Lerp(cam.m_Lens.FieldOfView, IsRunning() ? 75f : 60f, 10f * Time.deltaTime);
+        if (canMove) {
+            cameraController();
+        }
+    }
+
+    private void FixedUpdate() {
+        if (canMove) movementController();
+    }
+
+    private void cameraController() {
+        Vector2 mouse = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSens;
+        
+        playerT.Rotate(Vector3.up, mouse.x, Space.World);
+
+        cameraXRotation -= mouse.y;
+        cameraXRotation = Mathf.Clamp(cameraXRotation, cameraClamp.x, cameraClamp.y);
+
+        cam.transform.eulerAngles = new Vector3(cameraXRotation, cam.transform.eulerAngles.y, 0f);
+    }
+
+    public void MoveTo(Transform pos) {
+        transform.position = pos.position;
+    }
+
+    private void movementController() {
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+
+        ManageFootsteps();
+
+        float gravity = rb.velocity.y + Physics.gravity.y * gravityScale * Time.fixedDeltaTime;
+
+        if (IsGrounded() && rb.velocity.y < 0f) {
+            gravity = 0f;
+        }
+
+        float speed = walkingSpeed;
+
+        Vector3 direction = cam.transform.forward * input.y + cam.transform.right * input.x;
+        direction.y = 0f;
+        direction = direction.normalized * speed;
+
+        direction *= transform.localScale.magnitude;
+
+        rb.velocity = direction + Vector3.up * gravity;
+    }
+
+    void ManageFootsteps() {
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+
+        Vector3 velocity = rb.velocity;
+        velocity.y = 0f; // no footstep sounds in the air
+
+        footstepTime += velocity.magnitude * Time.deltaTime;
+        if (IsRunning()) footstepTime += input.magnitude * Time.deltaTime;
+
+        if (footstepTime >= footstepInterval) {
+            footstepTime = 0f;
+            //footstepManager.PlayFootstepSound();
+            //footstepAudioSource.PlayOneShot(footstepSounds[Random.Range(0, footstepSounds.Length)]);
+        }
+    }
+
+    bool IsRunning() {
+        return Input.GetKey(KeyCode.LeftShift) && Input.GetAxisRaw("Vertical") > 0f;
+    }
+
+    bool IsGrounded() {
+        return Physics.Raycast(transform.position, -Vector3.up, collider.bounds.extents.y + 0.15f, 1);
+    }
+}
