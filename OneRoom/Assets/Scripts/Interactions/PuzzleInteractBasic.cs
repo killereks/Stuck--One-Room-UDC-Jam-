8d9b14695b29281736fb7453fd8fab6b9116e9d1
killereks <ssneeff@gmail.com>
2021-09-11 20:31:57 +0100

    fix colliders on items, add pentagram, improve cauldron

--- OneRoom/Assets/Scripts/Interactions/PuzzleInteractBasic.cs
+++ OneRoom/Assets/Scripts/Interactions/PuzzleInteractBasic.cs
@@ -10,14 +10,21 @@ public class PuzzleInteractBasic : MonoBehaviour, IInteractable {
     public bool removeItemAfterUse;
     [Tooltip("What happens after player successfully interacts with this")]
     public UnityEvent outcome;
+    [Tooltip("This puzzle can only be done once")]
+    public bool oneTimeUse;
+
+    bool used;
 
     public void Interact() {
+        if (oneTimeUse && used) return;
+
         Item itemInHand = PlayerInventory.instance.ItemInHand();
 
         if (itemInHand == itemRequired) {
             if (removeItemAfterUse) {
                 PlayerInventory.instance.RemoveItem();
             }
+            used = true;
             outcome.Invoke();
         }
     }
